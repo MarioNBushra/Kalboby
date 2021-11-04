24846f20b76a9b0bbd3303eb1cce8719254d3075
MarioNBushra <mariobushra@outlook.com>
2021-11-04 10:35:59 +0200

    Maintain date

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,4 +1,6 @@
 let regDatee = document.getElementById("regDate")
 var today = new Date();
-var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-regDatee.value = date
+// var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+ today.getDay();
+var currentDate = today.toISOString().substring(0,10);
+
+regDatee.value = currentDate
