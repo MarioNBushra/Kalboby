0567597235d0d08e56bcaedec0f64c92055062a7
MarioNBushra <mariobushra@outlook.com>
2021-12-06 16:05:40 +0200

    Linux

--- test.js
+++ test.js
@@ -1,31 +1,19 @@
-// {
-//     "name": "template-hbs",
-//     "version": "1.0.0",
-//     "description": "",
-//     "main": "index.js",
-//     "scripts": {
-//       "start": "node src/server.js",
-//       "dev": "nodemon src/server.js"
-//     },
-//     "keywords": [],
-//     "author": "",
-//     "license": "ISC",
-//     "dependencies": {
-//       "axios": "^0.21.4",
-//       "bcrypt": "^5.0.1",
-//       "cookie-parser": "^1.4.5",
-//       "cors": "^2.8.5",
-//       "dotenv": "^10.0.0",
-//       "env-cmd": "^10.1.0",
-//       "express": "^4.17.1",
-//       "express-handlebars": "^5.3.0",
-//       "handlebars": "^4.7.7",
-//       "hbs": "^4.1.1",
-//       "jsonwebtoken": "^8.5.1",
-//       "mongoose": "^6.0.11",
-//       "nodemon": "^2.0.13",
-//       "puppeteer": "^10.4.0",
-//       "validator": "^13.6.0"
-//     }
-//   }
-  
+//Quiz: Checking your Balance (3-5)
+
+var balance = 350.2569;
+var checkBalance = true;
+var isActive = true;
+
+if (checkBalance === true){
+    if(isActive === true && balance > 0){
+        console.log("Your balance is $" + balance.toFixed(2) + ".");
+    }else if(isActive === false){
+        console.log("Your account is no longer active.");
+    }else if(isActive === true && balance === 0){
+        console.log("Your account is empty.");
+    }else if(isActive === true && balance < 0){
+        console.log("Your balance is negative. Please contact bank.");
+    }
+}else{
+    console.log("Thank you. Have a nice day!");
+}
